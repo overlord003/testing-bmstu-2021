fbf0404819db757767f1dc70ee3965665c1a0447
overlord003 <lovtsova.christina@yandex.ru>
2022-01-09 22:40:29 +0300

    test for getUsers() in user service added

--- src/app/data/fake-data.ts
+++ src/app/data/fake-data.ts
@@ -1,4 +1,4 @@
-const users = [
+const fakeUsers = [
     { id: 1, name: 'Christina', lastname: 'Lovtsova', role: 'Student' },
     { id: 2, name: 'Denis', lastname: 'Becasov', role: 'Teacher' },
     { id: 3, name: 'Igor', lastname: 'Rudakov', role: 'Teacher' }
--- src/app/services/user-service/user.service.spec.ts
+++ src/app/services/user-service/user.service.spec.ts
@@ -1,18 +1,49 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of} from 'rxjs';
 
+import { User } from 'src/app/models/user.model';
 import { UserService } from '../user-service/user.service';
+
+
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 
 describe('UserService', () => {
   let service: UserService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
+  let httpMock: HttpTestingController;
   
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
+    service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('#getUsers', () => {
+    it('should return an Observable<User[]>', () => {
+      const dummyUsers = [
+        { id: 0, name: 'First', lastname: 'User', role: 'Tester' },
+        { id: 1, name: 'Second', lastname: 'User', role: 'Tester' }
+      ];
+  
+      service.getUsers().subscribe(users => {
+        expect(users.length).toBe(2);
+        expect(users).toEqual(dummyUsers);
+      });
+  
+      const req = httpMock.expectOne('api/users');
+      expect(req.request.method).toBe("GET");
+
+      req.flush(dummyUsers);
+    });
+  });
+
 });
